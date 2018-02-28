10bfebd3d7701b8dc69b973e12c61658dc038a7f
Vinicius Roxo <viniroxo744@gmail.com>
2018-02-27 21:43:40 -0300

    First Commit

---
+++ SpaceKids/app/src/main/java/com/example/a44602569838/spacekids/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.a44602569838.spacekids;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
