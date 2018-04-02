f816d9d5d64453a3f20ee9c03bf306fe5282d4f4
Vinicius Roxo <viniroxo744@gmail.com>
2018-04-02 20:31:10 -0300

    - Pega saporra

--- SpaceKids/app/src/main/java/com/example/a44602569838/spacekids/adapters/RVAdapterCriancas.java
+++ SpaceKids/app/src/main/java/com/example/a44602569838/spacekids/adapters/RVAdapterCriancas.java
@@ -1,11 +1,15 @@
 package com.example.a44602569838.spacekids.adapters;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.a44602569838.spacekids.R;
+import com.example.a44602569838.spacekids.game.Fase1;
 import com.example.a44602569838.spacekids.model.Cricanca;
 
 import java.util.ArrayList;
@@ -24,23 +28,47 @@ public class RVAdapterCriancas extends RecyclerView.Adapter<RecyclerView.ViewHol
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_criancas, parent, false);
-        return null;
+        return new CriancasViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof CriancasViewHolder) {
+            CriancasViewHolder criancasViewHolder = (CriancasViewHolder) holder;
 
+            criancasViewHolder.nome.setText(cricancas.get(position).getNome());
+            criancasViewHolder.idade.setText(cricancas.get(position).getIdade());
+            criancasViewHolder.sexo.setText((cricancas.get(position).getSexo()));
+            criancasViewHolder.foto.setImageResource(cricancas.get(position).getSexo().equals("masculino") ? R.drawable.avatares_02 : R.drawable.avatares_01);
+
+            criancasViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i = new Intent(view.getContext(), Fase1.class);
+                    view.getContext().startActivity(i);
+                }
+            });
+
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return cricancas.size();
     }
 
-    public class CriancasViewHolder extends RecyclerView.ViewHolder{
+    public class CriancasViewHolder extends RecyclerView.ViewHolder {
+        ImageView foto;
+        TextView nome, idade, sexo;
 
         public CriancasViewHolder(View itemView) {
             super(itemView);
+
+            foto = itemView.findViewById(R.id.foto);
+            nome = itemView.findViewById(R.id.nomeCrianca);
+            idade = itemView.findViewById(R.id.idadeCrianca);
+            sexo = itemView.findViewById(R.id.sexoCrianca);
+
         }
     }
 }
--- SpaceKids/app/src/main/java/com/example/a44602569838/spacekids/model/Cricanca.java
+++ SpaceKids/app/src/main/java/com/example/a44602569838/spacekids/model/Cricanca.java
@@ -1,8 +1,104 @@
 package com.example.a44602569838.spacekids.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by 41183607806 on 22/03/2018.
  */
 
-public class Cricanca {
+public class Cricanca  implements Parcelable{
+    private String criancaId;
+    private String nome;
+    private String idade;
+    private String sexo;
+    private String foto;
+
+    public Cricanca() {
+    }
+
+    private Cricanca(Parcel in) {
+        criancaId = in.readString();
+        nome = in.readString();
+        idade = in.readString();
+        sexo = in.readString();
+        foto = in.readString();
+    }
+
+    public static final Creator<Cricanca> CREATOR = new Creator<Cricanca>() {
+        @Override
+        public Cricanca createFromParcel(Parcel in) {
+            return new Cricanca(in);
+        }
+
+        @Override
+        public Cricanca[] newArray(int size) {
+            return new Cricanca[size];
+        }
+    };
+
+    public String getCriancaId() {
+        return criancaId;
+    }
+
+    public void setCriancaId(String criancaId) {
+        this.criancaId = criancaId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    @Override
+    public String toString() {
+        return "Cricanca{" +
+                "criancaId='" + criancaId + '\'' +
+                ", nome='" + nome + '\'' +
+                ", idade='" + idade + '\'' +
+                ", sexo='" + sexo + '\'' +
+                ", foto='" + foto + '\'' +
+                '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(criancaId);
+        parcel.writeString(nome);
+        parcel.writeString(idade);
+        parcel.writeString(sexo);
+        parcel.writeString(foto);
+    }
 }
